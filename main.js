8c2c8e9c9d92fb34689110a352bd04da8cac099e
AanandBhandari <aanandbhandari143@gmail.com>
2019-04-09 13:57:52 +0545

    add mineBLOCK

--- main.js
+++ main.js
@@ -7,14 +7,23 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash()
+        this.noice = 0;
     }
     calculateHash() {
-        return SHA256(this.index + this.timeStamp + JSON.stringify(this.data) + this.previousHash).toString();
+        return SHA256(this.index + this.timeStamp + JSON.stringify(this.data) + this.previousHash +this.noice).toString();
+    }
+    mineBlock (difficulty) {
+        while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join('0')){
+            this.noice++;
+            this.hash = this.calculateHash()
+        }
+        console.log('Block mined :' + this.hash);   
     }
 }
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty  = 5; 
     }
     createGenesisBlock() {
         return new Block(0,'09/04/2019','GenesisBlock','0')
@@ -24,13 +33,28 @@ class Blockchain {
     }
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty)
         this.chain.push(newBlock);
+    }
+    isValidChain() {
+        for (let i = 1; i <this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+            return true;
+            
+        }
     }
 }
 
 let myCoin = new Blockchain();
-myCoin.addBlock(new Block(1, '10/09/2019', { amount: 4 }))
-myCoin.addBlock(new Block(2, '10/09/2019', { amount: 10 }))
 
-console.log(JSON.stringify(myCoin,null,4));
+console.log('block 1 is mining');
+myCoin.addBlock(new Block(1, '10/09/2019', { amount: 4 }))
+console.log('block 2 is mining');
+myCoin.addBlock(new Block(2, '10/09/2019', { amount: 10 }))
